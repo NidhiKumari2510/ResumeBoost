6e602d961599d2daf96d9fd832d8447b792aeb5d
NidhiKumari2510 <nidhikumari4963@gmail.com>
2026-06-16 15:40:10 +0530

    Complete authentication and backend integration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
